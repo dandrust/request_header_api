f2ca69d5b825e2ae432e455f4c1f11264d97e557
Dan <dandrust@gmail.com>
2016-08-03 22:05:59 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    var reqObj = JSON.stringify(req.headers);
+    var resObj = {}
+    resObj.language = req.headers['accept-language'].split(',')[0];
+    resObj.ipAddress = req.headers['x-forwarded-for']
+    resObj.software = req.headers['user-agent'].split(' (')[1].split(') ')[0];
+    
+    
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    
+    res.end(JSON.stringify(resObj));
+    
+    
+});
+
+app.listen(8080);
